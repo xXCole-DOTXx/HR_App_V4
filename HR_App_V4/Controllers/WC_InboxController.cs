1be9ee2b13447b9e1af0643257d4c97189255b61
xXCole-DOTXx <cole.k.perry@wv.gov>
2021-11-30 15:47:54 -0500

    Create page verified once

--- HR_App_V4/Controllers/WC_InboxController.cs
+++ HR_App_V4/Controllers/WC_InboxController.cs
@@ -54,7 +54,7 @@ namespace HR_App_V4.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,First_Name,Last_Name,Gender,Marital_Status,SSN,Address,Phone_Number,EmployeeID,Org_Number,Hire_Date,Job_Title,Work_Schedule,Injury_Date,Injury_Time,DOT_12,Start_Time,Injured_Body_Part,Side,Missing_Work,Missing_Work_Date,Begin_Missing_Date,Begin_Missing_Time,Return_To_Work_Date,Doctors_Release,Treatment,Treatment_Date,Treatment_Provider,Treatment_Provider_Phone,Transport_First_Treatment,Transport_City,Injury_Description,Equipment,Witness,Supervisor_Name,Supervisor_Phone,Questioned,Medical_History,Inbox_Submitted,Inbox_Reason,Comments,User_Email,Supervisor_Email,Safety_Specialist_Email,Optional_Email,Optional_Email2,Optional_Email3,HDHR_Manager_Email,Add_User,Date_Added")] WC_Inbox wC_Inbox)
+        public async Task<IActionResult> Create([Bind("ID,First_Name,Last_Name,Gender,Marital_Status,SSN,Address,Phone_Number,Org_Number,Hire_Date,Job_Title,Work_Schedule,Injury_Date,Injury_Time,Injury_Type,DOT_12,Start_Time,Injured_Body_Part,Side,Missing_Work,Missing_Work_Date,Begin_Missing_Date,Begin_Missing_Time,Return_To_Work_Date,Doctors_Release,Treatment,Treatment_Date,Treatment_Provider,Treatment_Provider_Phone,Transport_First_Treatment,Transport_City,Injury_Description,Equipment,Witness,Supervisor_Name,Supervisor_Phone,Questioned,Medical_History,Inbox_Submitted,Inbox_Reason,Comments,User_Email,Supervisor_Email,Safety_Specialist_Email,Optional_Email,Optional_Email2,Optional_Email3,HDHR_Manager_Email,Add_User,Date_Added")] WC_Inbox wC_Inbox)
         {
             DropDowns();
             if (ModelState.IsValid)
@@ -188,12 +188,65 @@ namespace HR_App_V4.Controllers
             List<SelectListItem> travelMethods = new()
             {
                 new SelectListItem { Value = "Self", Text = "Self" },
-                new SelectListItem { Value = "Ambulance", Text = "Ambulance" },
+                new SelectListItem { Value = "Land Ambulance", Text = "Land Ambulance" },
                 new SelectListItem { Value = "Coworker", Text = "Coworker" },
                 new SelectListItem { Value = "Other", Text = "Other" }
             };
             travelMethods.Insert(0, new SelectListItem { Value = null, Text = "Select" });
             ViewBag.travelMethods = travelMethods;
+
+            List<SelectListItem> sides = new()
+            {
+                new SelectListItem { Value = "Left", Text = "Left" },
+                new SelectListItem { Value = "Right", Text = "Right" },
+                new SelectListItem { Value = "Bilateral", Text = "Bilateral" },
+                new SelectListItem { Value = "NA", Text = "NA" }
+            };
+            sides.Insert(0, new SelectListItem { Value = null, Text = "Select" });
+            ViewBag.sides = sides;
+
+            List<SelectListItem> dot12Options = new()
+            {
+                new SelectListItem { Value = "Yes", Text = "Yes" },
+                new SelectListItem { Value = "No", Text = "No" },
+                new SelectListItem { Value = "Pending", Text = "Pending" }
+            };
+            dot12Options.Insert(0, new SelectListItem { Value = null, Text = "Select" });
+            ViewBag.dot12Options = dot12Options;
+
+            List<SelectListItem> schedules = new()
+            {
+                new SelectListItem { Value = "8.0 hours per day, 5 days per week", Text = "8.0 hours per day, 5 days per week" },
+                new SelectListItem { Value = "8.5 hours per day, 5 days per week", Text = "8.5 hours per day, 5 days per week" },
+                new SelectListItem { Value = "9.0 hours per day, 5 days per week", Text = "9.0 hours per day, 5 days per week" },
+                new SelectListItem { Value = "10.0 hours per day, 4 days per week", Text = "10.0 hours per day, 4 days per week" },
+                new SelectListItem { Value = "10.0 hours per day, 5 days per week", Text = "10.0 hours per day, 5 days per week" },
+                new SelectListItem { Value = "10.5 hours per day, 4 days per week", Text = "10.5 hours per day, 4 days per week" },
+                new SelectListItem { Value = "10.5 hours per day, 5 days per week", Text = "10.5 hours per day, 5 days per week" },
+                new SelectListItem { Value = "11.0 hours per day, 4 days per week", Text = "11.0 hours per day, 4 days per week" },
+                new SelectListItem { Value = "11.0 hours per day, 5 days per week", Text = "11.0 hours per day, 5 days per week" },
+                new SelectListItem { Value = "12.0 hours per day, 4 days per week", Text = "12.0 hours per day, 4 days per week" },
+                new SelectListItem { Value = "12.0 hours per day, 5 days per week", Text = "12.0 hours per day, 5 days per week" }
+            };
+            schedules.Insert(0, new SelectListItem { Value = null, Text = "Select" });
+            ViewBag.schedules = schedules;
+
+           /* List<SelectListItem> employmentTypes = new()
+            {
+                new SelectListItem { Value = "Full Time", Text = "Full Time" },
+                new SelectListItem { Value = "Temporary", Text = "Temporary" }
+            };
+            employmentTypes.Insert(0, new SelectListItem { Value = null, Text = "Select" });
+            ViewBag.employmentTypes = employmentTypes;
+
+            List<SelectListItem> claimResults = new()
+            {
+                new SelectListItem { Value = "Rejected", Text = "Rejected" },
+                new SelectListItem { Value = "Compensable", Text = "Compensable" },
+                new SelectListItem { Value = "Tolled", Text = "Tolled" }
+            };
+            claimResults.Insert(0, new SelectListItem { Value = null, Text = "Select" });
+            ViewBag.claimResults = claimResults;*/
         }
     }
 }
